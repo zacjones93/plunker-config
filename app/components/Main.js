d78b368b47387fc3779b62a233fad29cafe23da6
Zac Jones <zacjones93@gmail.com>
2016-07-18 13:08:20 -0700

    added functionality for showing html, JS, CSS, and Prev. Working on removing string if button is clicked again

--- app/components/Main.js
+++ app/components/Main.js
@@ -21,16 +21,17 @@ export default class Main extends React.Component{
 
   
 
-  handleClickHtml(showH, showJ){
+  handleClickHtml(showH, showJ, showC, showP){
     showH = !showH;
     this.state.ShowHTML =!this.state.ShowHTML 
+
     // checks if other tags are there, if not, adds '?show=index.html'
-    if (showH  && showJ == false){
+    if ( showH  && !showJ && !showC && !showP ){
       this.setState((state) => ({
-         url: state.url.concat("?show=index.html"),
+         url: state.url.concat(["?show=index.html"]),
         }))
-    } else if(showH == true && showJ == true){
-        this.setState((state) => ({ url: state.url.concat(",index.html") }))
+    } else if( showH && (showJ || showC || showP) ){
+        this.setState((state) => ({ url: state.url.concat([",index.html"]) }))
     }else if (!showH) {
       this.setState({
         url: this.state.baseUrl 
@@ -38,18 +39,69 @@ export default class Main extends React.Component{
     }
   }
 
-  handleClickJS(showJ){
+  handleClickJS(showH, showJ, showC, showP){
     showJ = !showJ;
     this.state.ShowJS = !this.state.ShowJS;
+
+    if ( showJ && !showH && !showC && !showP ){
+      this.setState((state) => ({
+        url: state.url.concat(["?show=js"]),
+      }))
+    } else if ( showJ && (showH || showC || showP) ) {
+      this.setState((state) => ({
+        url: state.url.concat([",js"]),
+      }))
+    }
+  }
+
+  handleClickCSS(showH, showJ, showC, showP){
+    showC = !showC;
+    this.state.ShowCSS = !this.state.ShowCSS;
+
+    if (showC && !showH && !showJ && !showP){
+      this.setState((state) => ({
+        url: state.url.concat(["?show=css"]),
+      }))
+    } else if (showC && (showH || showJ || showP) ) {
+      this.setState((state) => ({
+        url: state.url.concat([",css"]),
+      }))
+    }
+
+  }
+
+  handleClickPrev(showH, showJ, showC, showP){
+    showP = !showP;
+    this.state.ShowPrev = !this.state.ShowPrev
+
+    if (showP && !showH && !showJ && !showC){
+      this.setState((state) => ({
+        url: state.url.concat(["?show=preview"]),
+      }))
+    } else if (showP && (showH || showJ || showC) ) {
+      this.setState((state) => ({
+        url: state.url.concat([",preview"]),
+      }))
+    }
   }
 
   reset(){
-    this.setState((state) => ({ url: state.baseUrl }))
+    this.setState((state) => ({ 
+      url: state.baseUrl,
+      ShowHTML: false,
+      ShowJS: false,
+      ShowCSS: false,
+      ShowPrev: false
+    }))
   }
   
   render(){
+    // these variables are created because working directly with stat created
+    // issues due to its asyncronous nature
     var showH = this.state.ShowHTML;
     var showJ = this.state.ShowJS;
+    var showC = this.state.ShowCSS;
+    var showP = this.state.ShowPrev;
     return (
       <div>
         <div>
@@ -61,8 +113,11 @@ export default class Main extends React.Component{
         <div>
         
           <span>What tabs will be shown? (Defualts to index.html and preview)</span>
-          <button onClick={this.handleClickHtml.bind(this, showH, showJ)}>Show html</button>
-          <button onClick={this.handleClickJS.bind(this,showJ)}>Show JavaScript</button>
+          <button onClick={this.handleClickHtml.bind(this, showH, showJ, showC, showP)}>Show html</button>
+          <button onClick={this.handleClickJS.bind(this, showH, showJ, showC, showP)}>Show JavaScript</button>
+          <button onClick={this.handleClickCSS.bind(this, showH, showJ, showC, showP)}>Show CSS</button>
+          <button onClick={this.handleClickPrev.bind(this, showH, showJ, showC, showP)}>Show Preview</button>
+          
           <button onClick={this.reset.bind(this)}>reset</button>
         </div>
         <br/>
